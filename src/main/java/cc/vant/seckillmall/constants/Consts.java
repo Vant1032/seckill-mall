cc38d9a9e293082bc9a3ba16bd872512c4f8e32d
Vant <forvant@qq.com>
2020-04-24 02:06:38 +0800

    添加日期相关Consts

--- src/main/java/cc/vant/seckillmall/constants/Consts.java
+++ src/main/java/cc/vant/seckillmall/constants/Consts.java
@@ -11,4 +11,8 @@ public abstract class Consts {
     public static final String IS_USER_LOGIN = "isUserLogin";
 
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String TIME_FORMAT = "HH:mm:ss";
 }
