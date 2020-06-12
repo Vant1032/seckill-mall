788593ca6c24e9e4833bd48c548d23dc555c0521
Vant <forvant@qq.com>
2020-06-12 14:00:48 +0800

    优化pom
    优化代码

--- src/main/java/cc/vant/seckillmall/pojo/admin/req/UploadImageReq.java
+++ src/main/java/cc/vant/seckillmall/pojo/admin/req/UploadImageReq.java
@@ -3,8 +3,10 @@ package cc.vant.seckillmall.pojo.admin.req;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UploadImageReq {
-
+    @NotNull
     private MultipartFile file;
 }
